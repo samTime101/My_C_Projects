fcdbafa8aac817a98285187f07b9d675f914fcdd
samTime101 <srijanregmi44@gmail.com>
2023-08-12 16:22:47 +0545

    collatz

---
+++ collatz/collatz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int collatz(int);
+
+void main()
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    collatz(n);
+}
+
+int collatz(int n)
+{
+    long STEPS = 1;
+    do
+    {
+        STEPS++;
+
+        printf("%d\n", n);
+        // EVEN
+        if (n % 2 == 0)
+        {
+            n = n / 2;
+        }
+        // ODD
+        else
+        {
+            n = 3 * n + 1;
+        }
+        // FOR 1
+        if (n == 1)
+        {
+            printf("%d", n);
+        }
+
+    } while (n = !1);
+    printf("\n------NUMBER OF STEPS------\nSTEPS:%ld\n", STEPS);
+}
